98063cab40bdd03a2f89403e03ec8434916abb3b
lyndonteng <lyndon@live.com.sg>
2019-10-27 12:19:55 +0000

    Update test_adobe.py
    Added context_manager redirection for stdout

--- test_adobe.py
+++ test_adobe.py
@@ -1,39 +1,46 @@
 import socket
 import time
+# from [STT module] import f
 
 HOST = '127.0.0.1'
 PORT = 65432
 
+### this needs to go into the STT file
+from contextlib import redirect_stdout
+import io
 
-def main():
+f = io.StringIO()
+with redirect_stdout(f):
+    print()  # output of STT engine here3
+
+def main(f):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         time.sleep(5)
-        s.sendall(b'in')
-        time.sleep(1)
-        s.sendall(b'out')
-        time.sleep(1)
-        s.sendall(b'up')
-        time.sleep(1)
-        s.sendall(b'down')
-        time.sleep(1)
-        s.sendall(b'left')
-        time.sleep(1)
-        s.sendall(b'right')
-        time.sleep(1)
-        s.sendall(b'clockwise')
-        time.sleep(1)
-        s.sendall(b'anticlockwise')
-        time.sleep(1)
-        s.sendall(b'previous')
-        time.sleep(1)
-        s.sendall(b'next')
-        time.sleep(1)
-        s.sendall(b'width')
-        time.sleep(1)
-        s.sendall(b'page')
+        s.sendall(f)
+        # time.sleep(0.1)
+        # s.sendall(b'out')
+        # time.sleep(1)
+        # s.sendall(b'up')
+        # time.sleep(1)
+        # s.sendall(b'down')
+        # time.sleep(1)
+        # s.sendall(b'left')
+        # time.sleep(1)
+        # s.sendall(b'right')
+        # time.sleep(1)
+        # s.sendall(b'clockwise')
+        # time.sleep(1)
+        # s.sendall(b'anticlockwise')
+        # time.sleep(1)
+        # s.sendall(b'previous')
+        # time.sleep(1)
+        # s.sendall(b'next')
+        # time.sleep(1)
+        # s.sendall(b'width')
+        # time.sleep(1)
+        # s.sendall(b'page')
         s.close()
 
-
 if __name__ == '__main__':
-    main()
+    main(f)
